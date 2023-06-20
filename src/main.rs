ef5fa0e26cb77e4c17a2f8b7396aa86f0ae0f7e4
Marvin Hsu <marv.hsu@icloud.com>
2023-06-20 22:30:05 +0800

    add log

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,7 @@
 use axum::{http::StatusCode, routing::get, Router};
 use std::net::SocketAddr;
+use tower_http::trace::TraceLayer;
+use tracing_subscriber::{prelude::__tracing_subscriber_SubscriberExt, util::SubscriberInitExt};
 use utoipa::OpenApi;
 use utoipa_swagger_ui::SwaggerUi;
 
@@ -9,10 +11,20 @@ struct ApiDoc;
 
 #[tokio::main]
 async fn main() {
+    tracing_subscriber::registry()
+        .with(
+            tracing_subscriber::EnvFilter::try_from_default_env().unwrap_or_else(|_| {
+                "zero_to_product=debug,tower_http=debug,axum::rejection=trace".into()
+            }),
+        )
+        .with(tracing_subscriber::fmt::layer().json())
+        .init();
+
     let app = Router::new()
         .merge(SwaggerUi::new("/swagger-ui").url("/api-docs/openapi.json", ApiDoc::openapi()))
         .route("/", get(api::handler))
-        .route("/health_check", get(api::health_check));
+        .route("/health_check", get(api::health_check))
+        .layer(TraceLayer::new_for_http());
 
     let addr = SocketAddr::from(([0, 0, 0, 0], 3000));
 
@@ -23,13 +35,17 @@ async fn main() {
 }
 
 mod api {
+    use tracing::instrument;
+
     use super::*;
     #[utoipa::path(get, path = "/")]
+    #[instrument]
     pub async fn handler() -> &'static str {
         "Hello, world!"
     }
 
     #[utoipa::path(get, path = "/health_check")]
+    #[instrument]
     pub async fn health_check() -> StatusCode {
         StatusCode::OK
     }
