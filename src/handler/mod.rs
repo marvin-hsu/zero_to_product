1e95d1ed6f6947796976dc790b76f86ff7c2f607
Marvin Hsu <marvinhsu@nine-yi.com>
2023-07-06 17:33:54 +0800

    update openapi tag

--- src/handler/mod.rs
+++ src/handler/mod.rs
@@ -1,13 +1,13 @@
 use axum::http::StatusCode;
 use tracing::instrument;
 
-#[utoipa::path(get, path = "/")]
+#[utoipa::path(get, path = "/", tag = "basic")]
 #[instrument]
 pub async fn handler() -> &'static str {
     "Hello, world!"
 }
 
-#[utoipa::path(get, path = "/health_check")]
+#[utoipa::path(get, path = "/health_check", tag = "basic")]
 #[instrument]
 pub async fn health_check() -> StatusCode {
     StatusCode::OK
