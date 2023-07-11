4a730e4f50b98297b8a3cff7d290449e89f39ead
Marvin Hsu <marv.hsu@icloud.com>
2023-07-11 22:50:02 +0800

    add migration project

---
+++ migration/src/m20230711_142423_create_table.rs
@@ -0,0 +1,48 @@
+use sea_orm_migration::prelude::*;
+
+#[derive(DeriveMigrationName)]
+pub struct Migration;
+
+#[async_trait::async_trait]
+impl MigrationTrait for Migration {
+    async fn up(&self, manager: &SchemaManager) -> Result<(), DbErr> {
+        manager
+            .create_table(
+                Table::create()
+                    .table(Subscriptions::Table)
+                    .if_not_exists()
+                    .col(
+                        ColumnDef::new(Subscriptions::Id)
+                            .uuid()
+                            .not_null()
+                            .primary_key(),
+                    )
+                    .col(
+                        ColumnDef::new(Subscriptions::Email)
+                            .string()
+                            .not_null()
+                            .unique_key(),
+                    )
+                    .col(ColumnDef::new(Subscriptions::Name).string().not_null())
+                    .col(ColumnDef::new(Subscriptions::SubscribedAt).timestamp_with_time_zone())
+                    .to_owned(),
+            )
+            .await
+    }
+
+    async fn down(&self, manager: &SchemaManager) -> Result<(), DbErr> {
+        manager
+            .drop_table(Table::drop().table(Subscriptions::Table).to_owned())
+            .await
+    }
+}
+
+/// Learn more at https://docs.rs/sea-query#iden
+#[derive(Iden)]
+enum Subscriptions {
+    Table,
+    Id,
+    Email,
+    Name,
+    SubscribedAt,
+}
