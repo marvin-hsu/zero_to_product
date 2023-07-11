81d67ec5bfc1173cbb8d7db91dbb594ee58e1e5c
Marvin Hsu <marv.hsu@icloud.com>
2023-07-11 23:16:09 +0800

    generate entity

--- migration/src/lib.rs
+++ migration/src/lib.rs
@@ -1,6 +1,7 @@
 pub use sea_orm_migration::prelude::*;
 
 mod m20230711_142423_create_table;
+mod m20230711_145754_subscribedat_should_be_not_null;
 
 pub struct Migrator;
 
@@ -9,6 +10,7 @@ impl MigratorTrait for Migrator {
     fn migrations() -> Vec<Box<dyn MigrationTrait>> {
         vec![
             Box::new(m20230711_142423_create_table::Migration),
+            Box::new(m20230711_145754_subscribedat_should_be_not_null::Migration),
         ]
     }
 }
--- src/lib.rs
+++ src/lib.rs
@@ -1,11 +1,13 @@
 mod configuration;
 mod doc;
 mod domain;
+mod entity;
 mod handler;
 mod route;
 
 pub use configuration::*;
 pub use doc::*;
 pub use domain::*;
+pub use entity::*;
 pub use handler::*;
 pub use route::*;
