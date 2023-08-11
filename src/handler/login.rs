d2edada3e7705a69e3bb090b2b7133209578597c
Marvin Hsu <marv.hsu@icloud.com>
2023-08-11 23:18:40 +0800

    login handler

--- src/handler/login.rs
+++ src/handler/login.rs
@@ -1,18 +1,22 @@
+use crate::AppError::CustomWarning;
 use crate::{users, AppError, AppState};
 use axum::extract::State;
-use axum::http::StatusCode;
+use axum::http::{HeaderMap, StatusCode};
 use axum::Json;
+use axum_extra::extract::cookie::Cookie;
 use bcrypt::verify;
+use reqwest::header::SET_COOKIE;
 use sea_orm::{ColumnTrait, EntityTrait, QueryFilter};
 use serde::Deserialize;
 use tracing::instrument;
 use utoipa::ToSchema;
+
 #[derive(Deserialize, Debug, ToSchema)]
 pub struct User {
     /// UserName
-    pub user_name: Option<String>,
+    pub user_name: String,
     /// Password
-    pub password: Option<String>,
+    pub password: String,
 }
 
 #[utoipa::path(
@@ -27,9 +31,12 @@ pub struct User {
         (status = 400)
     ))]
 #[instrument]
-pub async fn login(state: State<AppState>, Json(user): Json<User>) -> Result<StatusCode, AppError> {
+pub async fn login(
+    state: State<AppState>,
+    Json(user): Json<User>,
+) -> Result<(StatusCode, HeaderMap), AppError> {
     let user_model = users::Entity::find()
-        .filter(users::Column::UserName.eq(user.user_name.unwrap()))
+        .filter(users::Column::UserName.eq(user.user_name))
         .one(&state.database)
         .await?
         .ok_or(AppError::CustomWarning(
@@ -37,11 +44,26 @@ pub async fn login(state: State<AppState>, Json(user): Json<User>) -> Result<Sta
             "Can't find User.".to_string(),
         ))?;
 
-    let valid = verify(user.password.unwrap(), &user_model.password.unwrap()).unwrap();
+    let valid = verify(user.password, &user_model.password.unwrap()).unwrap();
 
     if valid {
-        Ok(StatusCode::OK)
+        let token = state
+            .jwt_handler
+            .clone()
+            .create_token(&user_model.user_name);
+
+        let cookie = Cookie::build("token", token.to_owned())
+            .http_only(true)
+            .finish();
+
+        let mut headers = HeaderMap::new();
+        headers.insert(SET_COOKIE, cookie.to_string().parse().unwrap());
+
+        Ok((StatusCode::OK, headers))
     } else {
-        Ok(StatusCode::BAD_REQUEST)
+        Err(CustomWarning(
+            StatusCode::BAD_REQUEST,
+            "Error password".to_string(),
+        ))
     }
 }
